439ddd59b541a6ed4f44d8d434e26fae6ebeaaba
RAZAK1986 <144244667+RAZAK1986@users.noreply.github.com>
2023-09-06 19:16:48 +0530

    Add files via upload

---
+++ unit -1  challenges/main (1).py
@@ -0,0 +1,44 @@
+def check_leap_year(year):
+    """
+    Check if the given year is a leap year or not.
+    """
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print(f"{year} is a leap year")
+            else:
+                print(f"{year} is not a leap year")
+        else:
+            print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
+
+check_leap_year(2000)
+check_leap_year(2016)
+check_leap_year(2100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
